09a9b51c1eec964cb65d5510e8ee67ae5e15e018
VarunAV <avarun1@ced.alliance.edu.in>
2020-08-17 00:11:01 +0530

    added build

---
+++ precache-manifest.924b4cb9f545c6049c2a05586eb36767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be56f4c78204817a2bf1b7d83c2e41fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f72224a0ec4c103dfae",
+    "url": "/static/css/main.f4613f84.chunk.css"
+  },
+  {
+    "revision": "96c17c3b953541705ca6",
+    "url": "/static/js/2.1a17dc9d.chunk.js"
+  },
+  {
+    "revision": "c26bf70a7da39fcc2a44bc3e76fc17fb",
+    "url": "/static/js/2.1a17dc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f72224a0ec4c103dfae",
+    "url": "/static/js/main.d0fabad9.chunk.js"
+  },
+  {
+    "revision": "f9ad8a11efa2288d0809",
+    "url": "/static/js/runtime-main.f9473c76.js"
+  }
+]);
